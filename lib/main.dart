b1fad66ddcc3d7b567acb413d6aad7f169bffb93
abc <abc@example.com>
2023-10-09 16:27:58 +0300

    Adding custom todo item

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Poppins',
         brightness: Brightness.dark
       ),
       home: toDoView(),
--- lib/views/widgets/toso_view_body.dart
+++ lib/views/widgets/toso_view_body.dart
@@ -1,5 +1,9 @@
 import 'custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'custom_todo_icon.dart';
+
 class toDoViewBody extends StatelessWidget {
   const toDoViewBody({super.key});
 
@@ -9,11 +13,13 @@ class toDoViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 50,
+          ),
           customAppBar(),
+          customToDoItem(),
         ],
       ),
     );
   }
 }
-

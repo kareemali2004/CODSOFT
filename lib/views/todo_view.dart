3a06e195eee04adb346451d3103db400248327ba
abc <abc@example.com>
2023-10-09 18:17:04 +0300

    add floating button todo showbutton sheet

--- lib/views/todo_view.dart
+++ lib/views/todo_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:to_do_list/views/widgets/add_todo_button_sheet.dart';
 
 import 'package:to_do_list/views/widgets/toso_view_body.dart';
 
@@ -8,7 +10,13 @@ class toDoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showModalBottomSheet(context: context, builder: (context){
+            return  addTodoModelSheet();
+        });
+      },
+        child: Icon(FontAwesomeIcons.add),
+      ),
       body: toDoViewBody(),
     );
   }
---
+++ lib/views/widgets/add_todo_button_sheet.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class addTodoModelSheet extends StatelessWidget {
+  const addTodoModelSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

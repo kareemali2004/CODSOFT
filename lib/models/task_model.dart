268c60bbd9ff85e2f33ce1de400b8b380e75029b
abc <abc@example.com>
2023-10-11 19:00:29 +0300

    create type adaptor for task model

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,7 +1,14 @@
-class TaskModel {
+import 'package:hive/hive.dart';
+part 'task_model.g.dart';
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   TaskModel(

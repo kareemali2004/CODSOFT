f8750ddcde72f22b5ccf648f60a4778cf5bb7918
abc <abc@example.com>
2023-10-11 18:39:23 +0300

    add task model

---
+++ lib/models/task_model.dart
@@ -0,0 +1,12 @@
+class TaskModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  TaskModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
+}

17b2dc5d71cfa3f070dabc7cead1b5b899d12d35
abc <abc@example.com>
2023-10-14 15:17:21 +0300

    provider add task cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:to_do_list/constants.dart';
+import 'package:to_do_list/cubits/add_task_cubit/add_task_cubit.dart';
 import 'package:to_do_list/models/task_model.dart';
 import 'package:to_do_list/views/todo_view.dart';
 void main() async{
@@ -16,13 +18,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        brightness: Brightness.dark
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=>AddTaskCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+          brightness: Brightness.dark
+        ),
+        home: toDoView(),
+    
       ),
-      home: toDoView(),
-
     );}}
 

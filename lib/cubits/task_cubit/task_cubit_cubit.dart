be539a46767325a12a01da91e57868ba06ffb696
abc <abc@example.com>
2023-10-17 11:22:25 +0300

    create fetch tasks method

--- lib/cubits/task_cubit/task_cubit_cubit.dart
+++ lib/cubits/task_cubit/task_cubit_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:to_do_list/constants.dart';
 import 'package:to_do_list/models/task_model.dart';
 
 part 'task_cubit_state.dart';
 
-class TaskCubitCubit extends Cubit<TaskCubitState> {
-  TaskCubitCubit() : super(TaskCubitInitial());
+class TaskCubit extends Cubit<TaskCubitState>  {
+  TaskCubit() : super(TaskCubitInitial());
+  fetchAllTasks(){
+  
+    try {
+      var taskBox = Hive.box<TaskModel>(kTasksBox);
+     
+      emit(TaskCubitSucccess(taskBox.values.toList()));
+    } catch (e) {
+      emit(TaskCubitFailuer(e.toString()));
+    }
+  }
 }
--- lib/views/todo_view.dart
+++ lib/views/todo_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_list/cubits/task_cubit/task_cubit_cubit.dart';
 
 import 'package:to_do_list/views/widgets/add_todo_button_sheet.dart';
 
@@ -9,23 +11,27 @@ class toDoView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-
-        onPressed: () {
-        showModalBottomSheet(
-          isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30)
-            ),
-            context: context, builder: (context){
-            return  addTodoModelSheet();
-        });
-      },
-        child: Icon(Icons.check_box,size: 30,),
+    return BlocProvider(
+      create: (context) => TaskCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                context: context,
+                builder: (context) {
+                  return addTodoModelSheet();
+                });
+          },
+          child: Icon(
+            Icons.check_box,
+            size: 30,
+          ),
+        ),
+        body: toDoViewBody(),
       ),
-      body: toDoViewBody(),
     );
   }
 }
-
